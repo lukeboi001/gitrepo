9b274b1005d8cb31e8f547fdf39effad359ce7ba
lukeboi001 <lukasz.nowicki@lo1.sandomierz.pl>
2018-09-18 12:35:36 +0200

    funkcje

---
+++ python/funkcje.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  szablon.py
+
+# int() - przekształca argument na liczbę całkowitą
+# czyli typ INTEGER
+
+def hello():
+    print("Witaj w Python!")
+    
+def witaj():
+    imie = input("Jak masz na imię?")
+    print ("Witaj", imie,"!")
+    
+def suma(l1, l2):
+    print("Suma:", l1+l2)
+    
+def roznica(l1, l2):
+    print("Różnica:", l1-l2)
+    
+def iloczyn(l1, l2):
+    print("Iloczyn:", l1*l2)
+    
+def iloraz(l1, l2):
+    print("Iloraz:", l1/l2)
+        
+def main(args):
+    a = int(input("Podaj 1. liczbę: "))
+    print (a)
+    b = int(input ("Podaj 2. liczbę: "))
+    print (b)
+    
+    # print("Suma:", a + b)
+    suma(a, b)
+    # print("Różnica:", a - b)
+    roznica(a, b)
+    # print("Iloczyn:", a * b)
+    iloczyn(a, b) 
+    # print("Iloraz:", a / b)
+    iloraz(a, b) 
+    
+    hello()  # wywolanie funkcji
+    witaj()
+    
+    return 0
+    
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
--- python/wejscie-wyjscie.py
+++ python/wejscie-wyjscie.py
@@ -3,11 +3,20 @@
 #
 #  szablon.py
 
+# int() - przekształca argument na liczbę całkowitą
+# czyli typ INTEGER
+
 def main(args):
-    a = input("Podaj 1. liczbę: ")
+    a = int(input("Podaj 1. liczbę: "))
     print (a)
-    b = input ("Podaj 2. liczbę: ")
+    b = int(input ("Podaj 2. liczbę: "))
     print (b)
+    
+    print("Suma:", a + b)
+    print("Różnica:", a - b)
+    print("Iloczyn:", a * b)
+    print("Iloraz:", a / b)
+    
     return 0
     
 if __name__ == '__main__':
