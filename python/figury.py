13ebcdfdc17ea49e78961158041e69d5ff5917f1
lukeboi001 <lukasz.nowicki@lo1.sandomierz.pl>
2018-10-09 12:35:13 +0200

    figuły

--- python/figury.py
+++ python/figury.py
@@ -3,27 +3,44 @@
 #
 #  figury.py
 
-  
-
-def main(args):
-    
-    a = int(input("Podaj 1 bok: "))
-    b = int(input("Podaj 2 bok: "))
-    znak = input("Podaj znak: ")
+def prostokat1(a, b, znak):
     for i in range(a):
         for j in range(b):
             print(znak, end='')
         print()
+
         
-    print()
-        
+def prostokat2(a, b, znak):
     for i in range(a):
         for j in range(b):
-            if j == 0 or j == b -1:
+            if j == 0 or j == b -1 or i == 0 or i == a - 1:
                 print(znak, end='')
             else:
                 print(" ", end='')
         print()
+
+  
+def choinka1(h, znak):
+    for i in range(h):
+        for i in range(h):
+            if i == 0 or i == h + 1:
+                print(znak, end='')
+        print()
+    
+    
+
+    
+
+def main(args):
+    a = int(input("Podaj 1 bok: "))
+    b = int(input("Podaj 2 bok: "))
+    h = int(input("Podaj wysokość: "))
+    znak = input("Podaj znak: ")
+    prostokat1(a, b, znak)
+    print()
+    prostokat2(a, b, znak)
+    print()
+    choinka1(h, znak)
         
     return 0
 
