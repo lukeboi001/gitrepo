f3752d3a5525af9035e5017cd976df631f205179
lukeboi001 <lukasz.nowicki@lo1.sandomierz.pl>
2018-11-08 07:55:47 +0100

    znaki 2

--- cpp/znaki.cpp
+++ cpp/znaki.cpp
@@ -16,20 +16,52 @@ int zlicz(char tab[]) {
 void drukuj(char tab[], int roz) {
     for(int i=0; i<roz; i++) {
         cout << tab[i] << " ";
-        }
+    }
+}
+
+void ascii(char tab[], int roz) {
+    int kod = 0; //kod ascii znaku
+    for(int i=0; i<roz; i++) {
+        kod = (int)tab[i];
+        if (kod > 96 && kod < 123)
+            cout << (char)(kod-32) << " ";
+        else
+            cout << (int)tab[i] << " ";
+    }
 }
 
 void liczznaki(char tab[], int roz) {
     int spacje = 0;
     int interpunkcja = 0;
     int symbole = 0;
+    int reszta = 0;
     for(int i=0; i<roz; i++) {
-        if (tab[i] == ' ') spacje++;
-        else if (tab[i] == '.' || tab[i] == ',')
-            interpunkcja++;
-        else if (tab[i] == '(' || tab[i] == ')')
-            symbole++
+        //~if (tab[i] == ' ') spacje++;
+        //~else if (tab[i] == '.' || tab[i] == ',')
+            //~interpunkcja++;
+        //~else if (tab[i] == '(' || tab[i] == ')')
+            //~symbole++
+        switch (tab[i]) {
+            case ' ':
+            case '\t':
+                spacje++;
+            break;
+            case '.':
+            case ',':
+                interpunkcja++;
+            break;
+            case '(':
+            case ')':
+                symbole++;
+            break;
+            default:
+                reszta++;
+        }
     }
+    cout << "Spacje: " << spacje << endl;
+    cout << "Interpunkcja: " << interpunkcja << endl;
+    cout << "Symbole: " << symbole << endl;
+    cout << "Reszta: " << reszta << endl;
 }
 
 int main(int argc, char **argv)
@@ -40,7 +72,9 @@ int main(int argc, char **argv)
     // cin >> tab;
     cin.getline(tab, rozmiar);
     // cout << "Cześć " << tab << "!" << endl;
-	drukuj(tab, zlicz(tab));
+	ascii(tab, zlicz(tab));
+    cout << endl;
+    liczznaki(tab, zlicz(tab));
 	return 0;
 }
 
