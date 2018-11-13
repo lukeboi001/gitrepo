f272ea90c4b572a1f177514d8b2ca400134f6b03
lukeboi001 <lukasz.nowicki@lo1.sandomierz.pl>
2018-11-13 12:35:58 +0100

    anagramy

--- cpp/znaki.cpp
+++ cpp/znaki.cpp
@@ -25,6 +25,8 @@ void ascii(char tab[], int roz) {
         kod = (int)tab[i];
         if (kod > 96 && kod < 123)
             cout << (char)(kod-32) << " ";
+        else if (kod > 64 && kod < 91)
+            cout << (char)(kod+32) << " ";
         else
             cout << (int)tab[i] << " ";
     }
