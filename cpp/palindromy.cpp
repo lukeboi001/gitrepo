095406904e53a7c77bd45e4b36303c810074969e
lukeboi001 <lukasz.nowicki@lo1.sandomierz.pl>
2018-11-15 07:55:49 +0100

    palindromyyy

--- cpp/palindromy.cpp
+++ cpp/palindromy.cpp
@@ -8,12 +8,22 @@
 
 using namespace std;
 
-bool palindrom(
+bool palindrom(char w[], int r) {
+    bool czy_pal = true;
+    for (int i = 0; i < r/2; i++) {
+        if (w[i] != w[r - 1 - i]){
+            czy_pal = false;
+            break;
+        }
+    }
+    return czy_pal;
+    
+}
 
 int main(int argc, char **argv)
 {
     const int rozmiar = 20;
-    char wyraz[20] = "ryba";
+    char wyraz[rozmiar];
     cout << "Podaj wyraz lub zdanie: ";
     cin.getline(wyraz, rozmiar);
     cout << cin.gcount() << endl;
