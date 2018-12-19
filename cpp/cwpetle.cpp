388f259894c4fcc9e9876947f984ee78a67a8ba5
lukeboi001 <lukasz.nowicki@lo1.sandomierz.pl>
2018-12-19 08:45:10 +0100

    cwpetle

---
+++ cpp/cwpetle.cpp
@@ -0,0 +1,42 @@
+/*
+ * cwpetle.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+void cw01() {
+    int i = 0;
+    int a;
+    while (a <= 75) {
+    cout << "Wprowadź liczbę: ";
+    cin >> a;
+        i = i + a;
+        
+    }
+    cout << "Suma: " << i << endl;
+    
+}
+
+void cw02() {
+    int n;
+    int m;
+    for(n > 0; n < m; n++) {
+        
+    }
+    cout << "Podaj pierwszą liczbę: "
+    
+}
+
+void cw03() {
+    ;
+}
+
+int main(int argc, char **argv)
+{
+	cw01();
+    return 0;
+    
+}
+
