95134df843c010f8736d081e5c108bb25d4ef359
lukeboi001 <lukasz.nowicki@lo1.sandomierz.pl>
2018-12-20 07:55:25 +0100

    cwiczenia

---
+++ cpp/petle_cw01.cpp
@@ -0,0 +1,31 @@
+/*
+ * petle_cw01.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+void cw01() {
+    int j = 0;
+    for(int i = 10; i < 100; i++) {
+        if(i % 2  == 0)
+        j = j + i;
+        
+    }
+    cout << j << endl;
+}
+
+void cw02() {
+    int n = 0;
+    int m = 0;
+    for (
+    
+}
+
+int main(int argc, char **argv)
+{
+	cw01();
+	return 0;
+}
+
